2a351e5d154172010b86ee9143575355acd519d6
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-16 23:28:12 -0400

    Single file process complete

--- src/brain_dump/core.clj
+++ src/brain_dump/core.clj
@@ -6,7 +6,6 @@
 (defn db [src]
   (parse-stream (reader src)))
 
-
 (defn clean [s]
   (str/replace s #"^\[" ""))
 
@@ -14,11 +13,12 @@
   (reverse (sort-by count (remove #(= % "the") (map #(clean (get % "title")) db)))))
 
 (defn rgx [words]
-  (re-pattern (str "(" (str/join "|" words) ")")))
+  (re-pattern (str "\\b(" (str/join "|" words) ")\\b")))
 
 (defn transform [s pattern]
   (str/replace s pattern "[[$1]]"))
 
 (defn -main
-  [& args]
-  (println))
+  [roam-src input-md]
+  (spit (str "roam-" input-md)
+        (transform (slurp input-md) (rgx (references (db roam-src))))))
